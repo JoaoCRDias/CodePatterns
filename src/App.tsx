f73ece2ae065abc633c20fa99751945bcd2d97d6
João Carlos <joaocrdias17@gmail.com>
2021-12-15 17:07:31 -0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {View} from 'react-native';
+import HelloWorld from './components/HelloWorld';
+
+const App = () => {
+  return (
+    <View>
+      <HelloWorld />
+    </View>
+  );
+};
+
+export default App;
